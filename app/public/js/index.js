e1f582dbe2be6d2c5a5b12496cb53a68adbefb66
Reuel John K <reueljohn+github@pm.me>
2021-09-23 14:34:14 -0400

    Added refresh button

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -26,18 +26,22 @@ const Offer = {
         prettyBirthday() {
             return dayjs(this.person.dob.date).format("DD MMM YYYY")
         }
-
+    },
+    methods: {
+        fetchUserData() {
+            fetch('https://randomuser.me/api/')
+                .then(response => response.json())
+                .then((responseJson) => {
+                    console.log(responseJson);
+                    this.person = responseJson.results[0];
+                })
+                .catch((err) => {
+                    console.error(err);
+                })
+        }
     },
     created() {
-        fetch('https://randomuser.me/api/')
-            .then(response => response.json())
-            .then((responseJson) => {
-                console.log(responseJson);
-                this.person = responseJson.results[0];
-            })
-            .catch((err) => {
-                console.error(err);
-            })
+        this.fetchUserData();
     }
 }
 
